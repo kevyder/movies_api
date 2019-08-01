26542a58e8c9f925fd7bbc993be4b8c5a68a16f2
Kevin <kyrodriguez97@gmail.com>
2019-07-31 21:02:17 -0500

    User fields validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,32 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  has_secure_password
+  # Creation Callback
+  before_create :generate_token
+
+  validates :name, presence: true
+  # Email validations
+  validates :email,
+            presence: true,
+            format: {
+              with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+            },
+            uniqueness: {
+              message: 'User already exists'
+            }
+
+  # Password Validations
+  validates_confirmation_of :password
+  validates_presence_of :password_confirmation, if: -> { !password.nil? }
+
+  private
+
+  # User token generation.
+  def generate_token
+    self.token = loop do
+      token = SecureRandom.hex
+      break token unless User.exists?(token: token)
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "it's valid user name" do
+    assert users(:one).valid?
   end
+
+  test "it's not valid user name" do
+    user = users(:two)
+    user.name = nil
+    assert_not user.valid?
+  end
+
+  test "it's not valid user email format" do
+    user = users(:one)
+    user.email = 'anystring.com'
+    assert_not user.valid?
+  end
+
 end
