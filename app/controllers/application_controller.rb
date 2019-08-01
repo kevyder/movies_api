76175d0890c6d9f4b9d649d635fb9e777e335cd6
Kevin <kyrodriguez97@gmail.com>
2019-08-01 15:35:04 -0500

    HTTP Authentication by token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  def authenticate
+    # HTTP authentication by token
+    # key: Authorization
+    # value: Token token=[user token]
+    authenticate_or_request_with_http_token do |token, _options|
+      @current_user = User.find_by_token(token)
+    end
+  end
 end
