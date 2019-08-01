ec8c1fcefe69f87b784b84d9bcc3d36ee2a32f75
Kevin <kyrodriguez97@gmail.com>
2019-07-31 21:15:41 -0500

    User serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  email           :string
+#  password_digest :string
+#  token           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+
 class User < ApplicationRecord
   has_secure_password
   # Creation Callback
