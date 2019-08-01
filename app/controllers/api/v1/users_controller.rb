4ae8226a0d2200dbabbaaf788bab863a2737fea8
Kevin <kyrodriguez97@gmail.com>
2019-07-31 22:08:59 -0500

    Users Sign Up

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          user_json = UserSerializer.new(@user).serialized_json
+          render json: user_json
+        else
+          render json: { errors: @user.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.permit(:name, :email, :password, :password_confirmation)
+      end
+    end
+  end
+end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  email           :string
+#  password_digest :string
+#  token           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :email, :token, :created_at, :updated_at
